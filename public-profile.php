58f495244942d2a307526a69964d4990daac88ae
Cian Gallagher <cg1830@gmail.com>
2014-06-29 22:19:47 +0100

    Header update

--- public-profile.php
+++ public-profile.php
@@ -6,11 +6,11 @@
 		<meta name="viewport" content="width=device-width" />
 		<title>ZuluCreate</title>
 		<link rel='stylesheet' href='css/bootstrap.css?ver=3.9.1' type='text/css' media='all' />
-		<link rel='stylesheet' href='css/icons.css?ver=3.9.1' type='text/css' media='all' />
-		<link rel='stylesheet' href='css/custom.css?ver=3.9.1' type='text/css' media='all' />
-		<link rel='stylesheet' href='css/skins/lightblue.css?ver=3.9.1' type='text/css' media='all' />
-		<link rel='stylesheet' href='css/bootstrap-responsive.css?ver=3.9.1' type='text/css' media='all' />
 		<link rel='stylesheet' href='css/style.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/icons.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/custom.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/bootstrap-responsive.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/msblue.css' type='text/css' media='all' />
 		<link rel='stylesheet' href='css/bootstrap-grid.css' type='text/css' media='all' />
 		<script type='text/javascript' src='js/jquery.js?ver=1.11.0'></script>
 		<script type='text/javascript' src='js/jquery-migrate.min.js?ver=1.2.1'></script>
@@ -45,137 +45,59 @@
 	</head>
 	
 	<body class="home page page-id-5 page-template page-template-template-masonry-portfolio-php">
-		<div id="page">
-			<header id="masthead" role="banner">
-				<div class="navbar navbar-inverse">
-					<div class="navbar-inner">
-						<div class="container">
-							<div class="row-fluid">
-								<div class="span12">
-									<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">    <span class="icon-bar"></span>
-	<span class="icon-bar"></span>
-	<span class="icon-bar"></span>
+	<div id="page">
+		<header id="masthead" role="banner">
+			<div class="navbar navbar-inverse">
+				<div class="navbar-inner">
+					<div class="container">
+						<div class="row-fluid">
+							<div class="span12">
+								<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<a class="brand" href="index.html" title="Zulu Create" rel="home">Zulu Create</a>
 
-	</button><a class="brand" href="http://www.wowthemes.net/demo-serenity/" title="Serenity." rel="home">Zulucreate.</a>
+								<div class="nav-collapse collapse">
+									<ul id="menu-topmenu" class="nav navbar-nav">
+										<li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-5 current_page_item"><a href="index.html">Home</a></li>
+										<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="#">Gallery</a></li>
+										<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="job-search.html">Jobs</a></li>
+										<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="#">How It Works</a></li>
+									</ul>
 
-									<div class="nav-collapse collapse">
-										<ul id="menu-topmenu" class="nav">
-											<li id="menu-item-46" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-5 current_page_item"><a href="http://www.wowthemes.net/demo-serenity/">Home</a>
-											</li>
-											<li id="menu-item-54" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Work + <b class="caret"></b></a>
-
-												<ul class="dropdown-menu js-activated">
-													<li id="menu-item-158" class="nav-header menu-item menu-item-type-custom menu-item-object-custom"><a href="#">Masonry</a>
-													</li>
-													<li id="menu-item-119" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/masonry-2-columns/">2 columns</a>
-													</li>
-													<li id="menu-item-118" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/masonry-3-columns/">3 Columns</a>
-													</li>
-													<li id="menu-item-117" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/masonry-4-columns/">4 Columns</a>
-													</li>
-													<li id="menu-item-160" class="divider menu-item menu-item-type-custom menu-item-object-custom"><a href="#">divider</a>
-													</li>
-													<li id="menu-item-159" class="nav-header menu-item menu-item-type-custom menu-item-object-custom"><a href="#">Fixed</a>
-													</li>
-													<li id="menu-item-145" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/portfolio-fixed-2-columns/">2 Columns</a>
-													</li>
-													<li id="menu-item-144" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/portfolio-fixed-3/">3 Columns</a>
-													</li>
-													<li id="menu-item-143" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/portfolio-fixed-4/">4 Columns</a>
-													</li>
-													<!--.dropdown-->
-												</ul>
-											</li>
-											<li id="menu-item-55" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages + <b class="caret"></b></a>
-
-												<ul class="dropdown-menu js-activated">
-													<li id="menu-item-154" class="menu-item menu-item-type-custom menu-item-object-custom"><a href="http://www.wowthemes.net/demo-serenity/ourportfolio/washington-dc/">Single Project</a>
-													</li>
-													<li id="menu-item-41" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/about-us/">About Us</a>
-													</li>
-													<li id="menu-item-40" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/about-us-2/">About Us 2</a>
-													</li>
-													<li id="menu-item-39" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/services/">Services</a>
-													</li>
-													<li id="menu-item-38" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/knowledge-base/">Knowledge Base</a>
-													</li>
-													<li id="menu-item-153" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/testimonials/">Testimonials</a>
-													</li>
-													<li id="menu-item-152" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/simple-page/">Simple Page</a>
-													</li>
-													<li id="menu-item-157" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/404-error/">404 Error</a>
-													</li>
-													<!--.dropdown-->
-												</ul>
-											</li>
-											<li id="menu-item-57" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Features + <b class="caret"></b></a>
-
-												<ul class="dropdown-menu js-activated">
-													<li id="menu-item-185" class="nav-header menu-item menu-item-type-custom menu-item-object-custom"><a href="#">Shortcodes</a>
-													</li>
-													<li id="menu-item-184" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/columns/">Columns</a>
-													</li>
-													<li id="menu-item-183" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/boxes-panels-alerts/">Boxes, Panels, Alerts</a>
-													</li>
-													<li id="menu-item-190" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/tabs-accordions-tables/">Tabs, Accordions, Tables</a>
-													</li>
-													<li id="menu-item-193" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/pricing-boxes/">Pricing Boxes</a>
-													</li>
-													<li id="menu-item-198" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/buttons/">Buttons</a>
-													</li>
-													<li id="menu-item-217" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/miscellaneous/">Miscellaneous</a>
-													</li>
-													<li id="menu-item-239" class="divider menu-item menu-item-type-custom menu-item-object-custom"><a href="#">divider</a>
-													</li>
-													<li id="menu-item-238" class="nav-header menu-item menu-item-type-custom menu-item-object-custom"><a href="#">Settings</a>
-													</li>
-													<li id="menu-item-237" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/theme-options/">Theme Options</a>
-													</li>
-													<li id="menu-item-244" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/custom-types/">Custom Types</a>
-													</li>
-													<li id="menu-item-265" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/sidebars-widgets/">Sidebars &#038; Widgets</a>
-													</li>
-													<li id="menu-item-263" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/other/">Other</a>
-													</li>
-													<!--.dropdown-->
-												</ul>
-											</li>
-											<li id="menu-item-45" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/blog/">Blog</a>
-											</li>
-											<li id="menu-item-56" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Contact + <b class="caret"></b></a>
-
-												<ul class="dropdown-menu js-activated">
-													<li id="menu-item-44" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/basic-contact/">Default Contact</a>
-													</li>
-													<li id="menu-item-43" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/large-map/">Large Map</a>
-													</li>
-													<li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="http://www.wowthemes.net/demo-serenity/basic-contact-2/">Basic Contact</a>
-													</li>
-													<!--.dropdown-->
-												</ul>
-											</li>
-										</ul>
-									</div>
+									<ul id="sign-topmenu" class="nav navbar-nav pull-right">
+										<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="#">Sign up</a></li>
+										<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="#">Sign in</a></li>
+									</ul>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-		<!-- /site-navigation -->
-	</header>
-<!-- /masthead -->
-<!-- intro -->
-<div class="introtitle">
-	<div class="container">
-		<h1>
-			Web Design &#038; Photo Studio 
-		</h1>
-			<p>Based in Sidney, Australia, our <i>team of artists</i> specialize in
-				<br>
-		<b>Professional Photography</b> for companies and individuals.</p>
+			</div>
+			<!-- /site-navigation -->
+		</header>
+		<!-- /masthead -->
+
+		<!-- intro -->
+		<div class="introtitle">
+			<div class="container">
+				<div class="row-fluid">
+					<!--<div class="panel" style="padding: 0;">-->
+						<div class="span8">
+							<p style="text-align: center; color: #555; line-height: 35px; margin-bottom: auto;">Showcase, Connect &amp; Gain Exposure</p>
+						</div>
+						<div class="span4">
+							<a href="#" class="sbutton square shadow medium default" style="background: #2672EC; margin-bottom: auto; padding: 5px 15px;">Get Started</a>
+						</div>
+						<div class="clearfix"></div>
+					<!--</div>-->
+				</div>
+			</div>
 		</div>
-</div>
-<!-- /intro -->
+		<!-- /intro -->
 <!-- Main Content - Profile Page -->
 
 	<div class="row" style="margin-top:-60px;">
